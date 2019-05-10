45b48e1cbe52b659cb15d87722e1ef698f84d018
yenda <eric@status.im>
2019-05-10 02:00:10 +0200

    [fix] ipfs CID

--- src/status_im/ipfs/core.cljs
+++ src/status_im/ipfs/core.cljs
@@ -4,8 +4,8 @@
 
 ;; we currently use an ipfs gateway but this detail is not relevant
 ;; outside of this namespace
-(def ^:const ipfs-add-url "https://ipfs.infura.io:5001/api/v0/add")
-(def ^:const ipfs-cat-url "https://ipfs.infura.io/ipfs/")
+(def ipfs-add-url "https://ipfs.infura.io:5001/api/v0/add?cid-version=1")
+(def ipfs-cat-url "https://ipfs.infura.io/ipfs/")
 
 (fx/defn cat
   [cofx {:keys [hash on-success on-failure]}]
@@ -30,10 +30,11 @@
        :size Size})))
 
 (fx/defn add
+  "Add `value` on ipfs, and returns its b58 encoded CID"
   [cofx {:keys [value on-success on-failure]}]
   (let [formdata (doto (js/FormData.)
                    ;; the key is ignored so there is no need to provide one
-                   (.append nil value))]
+                   (.append "file" value))]
     {:http-raw-post (cond-> {:url  ipfs-add-url
                              :body formdata
                              :timeout-ms 5000
